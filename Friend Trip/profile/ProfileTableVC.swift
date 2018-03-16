bd46490ccc6ec56bb11d4f1532a2ae2072412751
lfernandosantos <lf.fernandodossantos@MacBook-Fernando.local>
2018-03-16 00:13:04 -0300

    Adiciona background gradiente. Adiciona load ao carregar a foto de perfil. Pega informações do data manager.

--- Friend Trip/profile/ProfileTableVC.swift
+++ Friend Trip/profile/ProfileTableVC.swift
@@ -7,17 +7,49 @@
 //
 
 import UIKit
+import Kingfisher
+import FacebookLogin
 
 class ProfileTableVC: UITableViewController {
+
+    @IBOutlet weak var imageViewProfile: UIImageView!
+    @IBOutlet weak var labelProfile: UILabel!
+    @IBOutlet weak var labelEmail: UILabel!
+    @IBOutlet weak var indicatorView: UIActivityIndicatorView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
+        setupViews()
 
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+        getUserInfo()
+
+        setGradientBackGround()
+    }
+
+    func getUserInfo() {
+
+        indicatorView.startAnimating()
+        
+        DataManager().checkAccessData(completion: { (success, accessToken) in
+
+            if let accessToken = accessToken{
+                LoginFBRequest().getUserInfo(accessToken: accessToken){ result, error in
+                    if let result = result{
+                        guard let userFace = UserFace(JSON: result) else {
+                            return
+                        }
+
+                        let userViewModel = ProfileViewModel(userFace: userFace)
+                        let urlImage = URL(string: userViewModel.imageURL)
+                        self.imageViewProfile.kf.setImage(with: urlImage)
+                        self.labelProfile.text = userViewModel.nameProfile
+                        self.labelEmail.text = userViewModel.emailProfile
+                    }
+                    self.indicatorView.stopAnimating()
+                }
+            }
+        })
     }
 
     override func didReceiveMemoryWarning() {
@@ -25,18 +57,40 @@ class ProfileTableVC: UITableViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func logout(_ sender: Any) {
+        let alertSheet = UIAlertController(title: "", message: "Deseja deslogar?", preferredStyle: .actionSheet)
+        alertSheet.addAction(UIAlertAction(title: "Logout", style: .destructive, handler: { action in
+            LoginManager().logOut()
+            self.dismiss(animated: true, completion: nil)
+        }))
+        alertSheet.addAction(UIAlertAction(title: "Cancelar", style: .cancel, handler: nil))
+
+        present(alertSheet, animated: true, completion: nil)
+
+    }
+
+    func setupViews() {
+        indicatorView.hidesWhenStopped = true
+    }
+
+    func setGradientBackGround(){
+        let gradientLayer = CAGradientLayer()
+        gradientLayer.frame = self.tableView.bounds
+
+        let colorTop = UIColor(named: "ColorOrangeGrandientTop")
+        let colorBottom = UIColor(named: "ColorPinkGradientBottom")
+        gradientLayer.colors =  [colorTop, colorBottom].map{$0?.cgColor}
+        gradientLayer.startPoint = CGPoint(x: 0.0, y: 0.5)
+        gradientLayer.endPoint = CGPoint(x: 0.5, y: 1.2)
+        let backgroundView = UIView(frame: self.tableView.bounds)
+        backgroundView.layer.addSublayer(gradientLayer)
+        self.tableView.backgroundView = backgroundView
+    }
+
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+        return .lightContent
+    }
     // MARK: - Table view data source
-
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
-    }
-
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
-    }
-
     /*
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
