2b1a0aa88e82fd4d245af114834f2611a69f15ba
lfernandosantos <lf.fernandodossantos@MacBook-Fernando.local>
2018-03-16 00:15:18 -0300

    Cria view model.

--- Friend Trip/profile/ProfileViewModel.swift
+++ Friend Trip/profile/ProfileViewModel.swift
@@ -6,4 +6,33 @@
 //  Copyright © 2018 LFSantos. All rights reserved.
 //
 
-import Foundation
+import UIKit
+import Kingfisher
+
+class ProfileViewModel {
+
+    let nameProfile: String
+    let emailProfile: String
+    let imageURL: String
+
+    init(userFace: UserFace) {
+
+        var stringName = ""
+        if let first = userFace.firstName {
+            stringName += first
+        }
+        if let last = userFace.lastName {
+            stringName += " \(last)"
+        }
+        self.nameProfile = stringName
+        self.emailProfile = userFace.email ?? " "
+        self.imageURL = userFace.picture?.data?.url ?? " "
+    }
+
+    func getImage () -> UIImageView{
+        let urlImage = URL(string: "https://beebom-redkapmedia.netdna-ssl.com/wp-content/uploads/2016/01/Reverse-Image-Search-Engines-Apps-And-Its-Uses-2016.jpg")
+        let image = UIImageView()
+        image.kf.setImage(with: urlImage)
+        return image
+    }
+}
