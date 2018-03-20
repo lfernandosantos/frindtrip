f1ecc33bab5a4956540c75d3b6d8969214fa25a3
lfernandosantos <lf.fernandodossantos@MacBook-Fernando.local>
2018-03-19 22:37:35 -0300

    Tela nova Trip.

---
+++ Friend Trip/Trip/model/Trip.swift
@@ -0,0 +1,13 @@
+//
+//  Trip.swift
+//  Friend Trip
+//
+//  Created by Luiz Fernando dos Santos on 18/03/2018.
+//  Copyright © 2018 LFSantos. All rights reserved.
+//
+
+import UIKit
+
+class Trip: NSObject {
+
+}
