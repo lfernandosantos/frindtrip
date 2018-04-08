1281100ac57ba05b0e596436df1777000a21cbca
lfernandosantos <lf.fernandodossantos@MacBook-Fernando.local>
2018-04-07 21:06:45 -0300

    Salva usuário na API.

--- Friend Trip/login/LoginPresenter.swift
+++ Friend Trip/login/LoginPresenter.swift
@@ -21,7 +21,7 @@ class LoginPresenter: NSObject, LoginButtonDelegate {
     func loginButtonDidCompleteLogin(_ loginButton: LoginButton, result: LoginResult) {
         switch result {
         case .success(let garanted, let declined, let token):
-            self.makeLogin(accessToken: token)
+            self.makeLoginFB(accessToken: token)
         case .failed(let error):
             self.viewLogin.showAlert(title: "", msg: "Erro ao realizar login. \nTente novamente!")
             print(error)
@@ -36,24 +36,43 @@ class LoginPresenter: NSObject, LoginButtonDelegate {
         print("logout")
     }
 
-    func makeLogin(accessToken: AccessToken) {
+    func makeLoginFB(accessToken: AccessToken) {
         LoginFBRequest().getUserInfo(accessToken: accessToken){ result, error in
+
             if let result = result{
-                let userFace = UserFace(JSON: result)
-                self.viewLogin.performSegue(withIdentifier: "goHome", sender: userFace)
+                if let userFace = UserFace(JSON: result) {
+                    //verifica se usuário possui nome email e foto
+                    if userFace.name == nil || userFace.picture?.data?.url == nil || userFace.email == nil {
+                        self.viewLogin.showAlert(title: "Erro", msg: "Não foi possível caputar todos seus dados do Facebook. \nVocê precisa ter nome, email e foto de perfil para logar!")
+                    } else {
+                        self.loginAPI(user: userFace)
+                    }
+                } else {
+                    self.viewLogin.showAlert(title: "Erro ao logar", msg: "Não foi possível logar com o Facebook. \nVerifique seus dados!")
+                }
+            } else {
+                self.viewLogin.showAlert(title: "Erro ao logar", msg: "Não foi possível logar com o Facebook. \nVerifique seus dados!")
+            }
+        }
+    }
+
+    func loginAPI(user: UserFace) {
+        let userRequest = UserViewModel(userFace: user)
+        APIUser(user: userRequest).createNewUser { success, response, error in
+            if success {
+                self.viewLogin.performSegue(withIdentifier: "goHome", sender: user)
+            } else {
+                self.viewLogin.showAlert(title: "Ops", msg: "Problemas ao conectar com o servidor!\nVerifique sua conexão e tente novamente!")
             }
         }
     }
 
     func checkAlreadyLogin() {
         if let accessToken = AccessToken.current {
-            makeLogin(accessToken: accessToken)
+            makeLoginFB(accessToken: accessToken)
         }else{
+            viewLogin.stopIndicator()
             print("sem login")
         }
-    }
-
-    func createUser() {
-
     }
 }
