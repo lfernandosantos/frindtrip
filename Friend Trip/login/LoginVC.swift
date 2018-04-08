64c0618d4014f48a3a230839f1b83c866ee49ebc
lfernandosantos <lf.fernandodossantos@MacBook-Fernando.local>
2018-04-07 21:07:28 -0300

    Adiciona load na tela de login.

--- Friend Trip/login/LoginVC.swift
+++ Friend Trip/login/LoginVC.swift
@@ -12,7 +12,10 @@ import FacebookLogin
 class LoginVC: UIViewController {
 
     @IBOutlet var loginFBbtn: LoginButton!
+
     var loginVM: LoginPresenter?
+    var spinnerView = UIView()
+    let loadIndicator = UIActivityIndicatorView()
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,21 +23,48 @@ class LoginVC: UIViewController {
         loginVM = LoginPresenter(viewLogin: self)
         loginFBbtn = LoginButton(readPermissions: [ .publicProfile, .email, .userFriends, .userPhotos ])
         loginFBbtn.delegate = loginVM
+
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        setupLoading()
     }
 
     override func viewDidAppear(_ animated: Bool) {
+        startIndicator()
         loginVM?.checkAlreadyLogin()
     }
 
+    func setupLoading() {
+        spinnerView = UIView(frame: view.bounds)
+        spinnerView.backgroundColor = UIColor(named: "ColorDisable")
+        loadIndicator.center = spinnerView.center
+        loadIndicator.hidesWhenStopped = true
+        loadIndicator.activityIndicatorViewStyle = .whiteLarge
+        loadIndicator.startAnimating()
+        spinnerView.addSubview(loadIndicator)
+    }
+
+    func startIndicator() {
+        view.addSubview(spinnerView)
+    }
+
+    func stopIndicator() {
+        loadIndicator.stopAnimating()
+        spinnerView.removeFromSuperview()
+    }
+
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        stopIndicator()
         if let homeVC: HomeVC = segue.destination as? HomeVC{
             homeVC.user = sender as? UserFace
         }
     }
     
     func showAlert(title: String, msg: String){
-        let alert = UIAlertController(title: "Alert", message: "Message", preferredStyle: UIAlertControllerStyle.alert)
-        alert.addAction(UIAlertAction(title: "Click", style: UIAlertActionStyle.default, handler: nil))
+        stopIndicator()
+        let alert = UIAlertController(title: title, message: msg, preferredStyle: UIAlertControllerStyle.alert)
+        alert.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.default, handler: nil))
         self.present(alert, animated: true, completion: nil)
     }
 }
