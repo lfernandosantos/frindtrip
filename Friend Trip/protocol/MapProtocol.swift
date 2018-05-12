19e0c23e1b5e59556e066ed84a0383809c14e0e0
lfernandosantos <lf.fernandodossantos@MacBook-Fernando.local>
2018-05-12 19:55:12 -0300

    Cria protocolo.

---
+++ Friend Trip/protocol/MapProtocol.swift
@@ -0,0 +1,13 @@
+//
+//  MapProtocol.swift
+//  Friend Trip
+//
+//  Created by Luiz Fernando dos Santos on 12/05/2018.
+//  Copyright © 2018 LFSantos. All rights reserved.
+//
+
+import Foundation
+
+protocol MapProtocol {
+    func setCategory(category: String)
+}
