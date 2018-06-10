37873e3e3cfd5771db1571e2cd2a15370a963b72
lfernandosantos <lf.fernandodossantos@MacBook-Fernando.local>
2018-06-10 14:26:41 -0300

    Create get profile picture from facebook url.

---
+++ Friend Trip/Constants.swift
@@ -0,0 +1,15 @@
+//
+//  Constants.swift
+//  Friend Trip
+//
+//  Created by Luiz Fernando dos Santos on 10/06/2018.
+//  Copyright © 2018 LFSantos. All rights reserved.
+//
+
+import Foundation
+
+struct FaceConstants {
+
+    static let urlGraph: String = "https://graph.facebook.com/"
+    static let endPointPicLarge: String = "/picture?type=large"
+}
--- Friend Trip/model/UserFace.swift
+++ Friend Trip/model/UserFace.swift
@@ -11,7 +11,7 @@ import ObjectMapper
 
 class UserFace: NSObject, Mappable {
 
-    var id: String?
+    var id: Int?
     var name: String?
     var email:String?
     var picture: UserPicture?
@@ -24,5 +24,13 @@ class UserFace: NSObject, Mappable {
         email <- map["email"]
         picture <- map["picture"]
     }
-   
+
+    func getProfilePic() -> String {
+        guard let id = id else {
+            return ""
+        }
+        let urlPic: String = "\(FaceConstants.urlGraph)\(id)\(FaceConstants.endPointPicLarge)"
+        return urlPic
+    }
+
 }
