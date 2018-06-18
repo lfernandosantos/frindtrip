0736e6d032efeeb51f6b3789e56969ada87c4a32
lfernandosantos <lf.fernandodossantos@MacBook-Fernando.local>
2018-06-17 22:24:02 -0300

    Get uiimage category.

--- Friend Trip/UIImageCategory.swift
+++ Friend Trip/UIImageCategory.swift
@@ -6,4 +6,43 @@
 //  Copyright © 2018 LFSantos. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+class UIImageCategory {
+    static func getIconCategory(_ category: String) -> UIImage? {
+
+        if category == "Beer" {
+            return UIImage(named: ConstantsNamedImages.categoryBeer)
+        }
+        if category == "Adventure" {
+            return UIImage(named: ConstantsNamedImages.categoryAdventure)
+        }
+        if category == "Beach" {
+            return UIImage(named: ConstantsNamedImages.categoryBeach)
+        }
+        if category == "Party" {
+            return UIImage(named: ConstantsNamedImages.categoryParty)
+        } else {
+            return UIImage(named: ConstantsNamedImages.categoryAdventure)
+        }
+    }
+
+    static func getImgCategory(_ category: String) -> UIImage? {
+
+        if category == "Beer" {
+            return UIImage(named: ConstantsNamedImages.imgCategoryBeer)
+        }
+        if category == "Adventure" {
+            return UIImage(named: ConstantsNamedImages.imgCategoryAdventure)
+        }
+        if category == "Beach" {
+            return UIImage(named: ConstantsNamedImages.imgCategoryBeach)
+        }
+        if category == "Party" {
+            return UIImage(named: ConstantsNamedImages.imgCategoryParty)
+        } else {
+            return UIImage(named: ConstantsNamedImages.imgCategoryAdventure)
+        }
+    }
+    
+}
