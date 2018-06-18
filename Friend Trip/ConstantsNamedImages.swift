bb7bba77c85d3409428369038e27eb2c8f1e8221
lfernandosantos <lf.fernandodossantos@MacBook-Fernando.local>
2018-06-17 21:39:21 -0300

    Create constants.

--- Friend Trip/ConstantsNamedImages.swift
+++ Friend Trip/ConstantsNamedImages.swift
@@ -7,3 +7,17 @@
 //
 
 import Foundation
+
+
+struct ConstantsNamedImages {
+    static let categoryBeach = "ic_beach"
+    static let categoryParty = "ic_party"
+    static let categoryBeer = "ic_beer"
+    static let categoryAdventure = "ic_adventure"
+
+    static let imgCategoryBeach = "img_beach"
+    static let imgCategoryParty = "img_party"
+    static let imgCategoryBeer = "img_beer"
+    static let imgCategoryAdventure = "img_adventure"
+}
+
--- Friend Trip/GlobalConstants.swift
+++ Friend Trip/GlobalConstants.swift
@@ -7,3 +7,18 @@
 //
 
 import Foundation
+
+struct GlobalConstants {
+
+
+    struct SegueIdentifier {
+        static let CONFIRMED_TRIPS = "confirmedTrips"
+        static let SAVED_TRIPS = "savedTrips"
+        static let MY_TRIPS = "myTrips"
+        enum TypeTripsList {
+            case CONFIRMED
+            case SAVED
+            case MYTRIP
+        }
+    }
+}
