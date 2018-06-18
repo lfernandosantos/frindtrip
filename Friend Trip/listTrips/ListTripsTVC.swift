7373a31da97af45b38b5f54518ec00e76eb06375
lfernandosantos <lf.fernandodossantos@MacBook-Fernando.local>
2018-06-17 22:40:38 -0300

    Create list trips.

--- Friend Trip/listTrips/ListTripsTVC.swift
+++ Friend Trip/listTrips/ListTripsTVC.swift
@@ -11,16 +11,25 @@ import UIKit
 class ListTripsTVC: UITableViewController {
 
     var tripList = [TripsDAO] ()
+    var typeList: GlobalConstants.SegueIdentifier.TypeTripsList = .SAVED
+    @IBOutlet var userVM: UserViewModel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        tripList = UserViewModel.savedTrips()
-
+        switch typeList {
+        case .SAVED:
+            tripList = UserViewModel.savedTrips()
+        case .MYTRIP:
+            self.tripList = userVM.myTrips()
+            self.tableView.reloadData()
+        case .CONFIRMED:
+            self.tripList = UserViewModel.confirmedTrips()
+        }
+        
     }
 
     // MARK: - Table view data source
-
     override func numberOfSections(in tableView: UITableView) -> Int {
         return 1
     }
@@ -28,58 +37,50 @@ class ListTripsTVC: UITableViewController {
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return tripList.count
     }
-
-
+   
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "listCell", for: indexPath)
-        cell.textLabel?.text = tripList[indexPath.row].nome
+        let cell = tableView.dequeueReusableCell(withIdentifier: "listCell", for: indexPath) as! CustomSavedTVCell
+        cell.lblNomeTrip.text = tripList[indexPath.row].nome
+        cell.lblDataTrip.text = tripList[indexPath.row].data
+        cell.btnConfirmar.layer.cornerRadius = 20
+        cell.btnConfirmar.layer.borderWidth = 2
+        cell.btnConfirmar.layer.borderColor = UIColor.orange.cgColor
+        if let category = tripList[indexPath.row].tipoEvento {
+            cell.imgCategoria.image = getImgCategory(category)
+        }
+
         return cell
     }
 
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let item = tripList[indexPath.row]
+        performSegue(withIdentifier: "tripDetails", sender: item)
     }
-    */
 
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
+        if let detailVC = segue.destination as? DetailTripVC {
+            if let tripDao = sender as? TripsDAO {
+                let trip = Trip(tripDao: tripDao)
+                detailVC.tripViewModel = TripViewModel(trip: trip)
+            }
 
+        }
+    }
+    func getImgCategory(_ category: String) -> UIImage? {
+
+        if category == "Beer" {
+            return UIImage(named: ConstantsNamedImages.categoryBeer)
+        }
+        if category == "Adventure" {
+            return UIImage(named: ConstantsNamedImages.categoryAdventure)
+        }
+        if category == "Beach" {
+            return UIImage(named: ConstantsNamedImages.categoryBeach)
+        }
+        if category == "Party" {
+            return UIImage(named: ConstantsNamedImages.categoryParty)
+        } else {
+            return UIImage(named: ConstantsNamedImages.categoryAdventure)
+        }
+    }
 }
