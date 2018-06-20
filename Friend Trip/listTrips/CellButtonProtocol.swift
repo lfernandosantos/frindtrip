22b25ef1ed450904daae8cdb4ee243a748a4ecaa
lfernandosantos <lf.fernandodossantos@MacBook-Fernando.local>
2018-06-19 22:48:16 -0300

    Add assets.

---
+++ Friend Trip/listTrips/CellButtonProtocol.swift
@@ -0,0 +1,9 @@
+//
+//  CellButtonProtocol.swift
+//  Friend Trip
+//
+//  Created by Luiz Fernando dos Santos on 19/06/2018.
+//  Copyright © 2018 LFSantos. All rights reserved.
+//
+
+import Foundation
