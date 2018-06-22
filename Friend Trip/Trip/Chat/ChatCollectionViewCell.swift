46c6bb863ee28b1cf6e036aaf661649857a69d1c
lfernandosantos <lf.fernandodossantos@MacBook-Fernando.local>
2018-06-22 03:07:27 -0300

    Update assets.

---
+++ Friend Trip/Trip/Chat/ChatCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  ChatCollectionViewCell.swift
+//  Friend Trip
+//
+//  Created by Luiz Fernando dos Santos on 21/06/2018.
+//  Copyright © 2018 LFSantos. All rights reserved.
+//
+
+import UIKit
+
+class ChatCollectionViewCell: UICollectionViewCell {
+    
+}
---
+++ Friend Trip/Trip/Chat/MessageModel.swift
@@ -0,0 +1,9 @@
+//
+//  MessageModel.swift
+//  Friend Trip
+//
+//  Created by Luiz Fernando dos Santos on 21/06/2018.
+//  Copyright © 2018 LFSantos. All rights reserved.
+//
+
+import Foundation
