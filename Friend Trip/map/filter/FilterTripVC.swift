e82f5baa938dd698d37d1f1010b9076458fa673e
lfernandosantos <lf.fernandodossantos@MacBook-Fernando.local>
2018-07-01 22:43:50 -0300

    Create Filter view controller.

--- Friend Trip/map/filter/FilterTripVC.swift
+++ Friend Trip/map/filter/FilterTripVC.swift
@@ -8,28 +8,75 @@
 
 import UIKit
 
-class FilterTripVC: UIViewController {
+class FilterTripVC: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
+    @IBOutlet weak var calendarCollection: UICollectionView!
+    let months = ["Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho", "Julho", "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro"]
 
-        // Do any additional setup after loading the view.
+    let daysOfMonth = ["Domingo","Segunda", "Terça", "Quarta", "Quinta", "Sexta", "Sábado"]
+    let days = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+
+    var currentMonth = String()
+
+    @IBOutlet weak var monthLabel: UILabel!
+
+
+    @IBAction func nextMonth(_ sender: Any) {
+        switch currentMonth {
+        case "Dezembro":
+            month = 0
+            year += 1
+            updateMonth()
+            monthLabel.text = "\(currentMonth)"
+            calendarCollection.reloadData()
+        default:
+            month += 1
+            updateMonth()
+            monthLabel.text = "\(currentMonth)"
+            print(currentMonth)
+            calendarCollection.reloadData()
+        }
+
     }
+    
+    @IBAction func backMonth(_ sender: Any) {
+        switch currentMonth {
+        case "Janeiro":
+            month = 11
+            year -= 1
+            updateMonth()
+            monthLabel.text = "\(currentMonth)"
+            calendarCollection.reloadData()
+        default:
+            month -= 1
+            updateMonth()
+            monthLabel.text = "\(currentMonth)"
+            calendarCollection.reloadData()
+        }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
     
 
-    /*
-    // MARK: - Navigation
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        currentMonth = months[month]
+        monthLabel.text = currentMonth
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+        print(currentMonth)
     }
-    */
+
+    func updateMonth() {
+        currentMonth = months[month]
+    }
+
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return days[month]
+    }
+
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "calendarView", for: indexPath) as! DateCollectionViewCell
+        cell.dayDateLabel.text = "\(indexPath.row + 1)"
+        return cell
+    }
 
 }
