f1533ab57fdfe0f39dbfb287515e19540fde77dd
Fernando <fernando@Mac-mini.local>
2018-07-13 20:51:08 -0300

    Make calendar on filter.

--- Friend Trip/map/filter/FilterTripVC.swift
+++ Friend Trip/map/filter/FilterTripVC.swift
@@ -14,9 +14,21 @@ class FilterTripVC: UIViewController, UICollectionViewDelegate, UICollectionView
     let months = ["Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho", "Julho", "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro"]
 
     let daysOfMonth = ["Domingo","Segunda", "Terça", "Quarta", "Quinta", "Sexta", "Sábado"]
-    let days = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+    var days = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
 
     var currentMonth = String()
+    
+    var numberOfEmptyBox = Int()
+    
+    var nextNumberOfEmptyBox = Int()
+    
+    var PreviusNumberOfEmptyBox = 0
+    
+    var direction = 0
+    
+    var positionIndex = 0
+    
+    var leapYearCounter = 2
 
     @IBOutlet weak var monthLabel: UILabel!
 
@@ -26,12 +38,33 @@ class FilterTripVC: UIViewController, UICollectionViewDelegate, UICollectionView
         case "Dezembro":
             month = 0
             year += 1
+            direction = 1
+            
+            if leapYearCounter < 5 {
+                leapYearCounter += 1
+            }
+            
+            if leapYearCounter == 4 {
+                days[1] = 29
+            }
+            
+            if leapYearCounter == 5 {
+                leapYearCounter = 1
+                days[1] = 28
+            }
+            
+            getStartDateDayPosition()
             updateMonth()
             monthLabel.text = "\(currentMonth)"
             calendarCollection.reloadData()
         default:
+            direction = 1
+
+            getStartDateDayPosition()
+            
             month += 1
             updateMonth()
+            
             monthLabel.text = "\(currentMonth)"
             print(currentMonth)
             calendarCollection.reloadData()
@@ -44,11 +77,31 @@ class FilterTripVC: UIViewController, UICollectionViewDelegate, UICollectionView
         case "Janeiro":
             month = 11
             year -= 1
+            direction = -1
+            
+            if leapYearCounter > 0 {
+                leapYearCounter -= 1
+            }
+            
+            if leapYearCounter == 0 {
+                days[1] = 29
+                leapYearCounter = 4
+            } else {
+                days[1] = 28
+            }
+            
+            
+            
+            
+            getStartDateDayPosition()
             updateMonth()
             monthLabel.text = "\(currentMonth)"
             calendarCollection.reloadData()
         default:
             month -= 1
+            direction = -1
+            
+            getStartDateDayPosition()
             updateMonth()
             monthLabel.text = "\(currentMonth)"
             calendarCollection.reloadData()
@@ -56,6 +109,38 @@ class FilterTripVC: UIViewController, UICollectionViewDelegate, UICollectionView
 
     }
     
+    func getStartDateDayPosition() {
+        switch direction {
+        case 0:
+            switch day {
+            case 1...7:
+                numberOfEmptyBox = weekday - day
+            case 8...14:
+                numberOfEmptyBox = weekday - day - 7
+            case 15...21:
+                numberOfEmptyBox = weekday - day - 14
+            case 22...28:
+                numberOfEmptyBox = weekday - day - 21
+            case 29...31:
+                numberOfEmptyBox = weekday - day - 28
+            default:
+                break
+            }
+        case 1...:
+            nextNumberOfEmptyBox = (positionIndex + days[month])%7
+            positionIndex = nextNumberOfEmptyBox
+            
+        case -1:
+            PreviusNumberOfEmptyBox = (7 - (days[month] - positionIndex)%7)
+            if PreviusNumberOfEmptyBox == 7 {
+                PreviusNumberOfEmptyBox = 0
+            }
+            positionIndex = PreviusNumberOfEmptyBox
+            
+        default:
+            fatalError()
+        }
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -70,13 +155,65 @@ class FilterTripVC: UIViewController, UICollectionViewDelegate, UICollectionView
     }
 
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        switch direction {
+        case 0:
+            return days[month] + numberOfEmptyBox
+        case 1...:
+            return days[month] + nextNumberOfEmptyBox
+        case -1:
+            return days[month] + PreviusNumberOfEmptyBox
+        default:
+            fatalError()
+        }
         return days[month]
     }
 
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "calendarView", for: indexPath) as! DateCollectionViewCell
-        cell.dayDateLabel.text = "\(indexPath.row + 1)"
+        cell.backgroundColor = UIColor.clear
+        
+        cell.dayDateLabel.textColor = UIColor.black
+        
+        if cell.isHidden {
+            cell.isHidden = false
+        }
+        switch direction {
+        case 0:
+            cell.dayDateLabel.text = "\(indexPath.row + 1 - numberOfEmptyBox)"
+        case 1:
+            cell.dayDateLabel.text = "\(indexPath.row + 1 - nextNumberOfEmptyBox)"
+        case -1:
+            cell.dayDateLabel.text = "\(indexPath.row + 1 - PreviusNumberOfEmptyBox)"
+        default:
+            fatalError()
+        }
+        
+        if Int(cell.dayDateLabel.text!)! < 1 {
+            cell.isHidden = true
+        }
+        
+        switch indexPath.row {
+        case 0,6,7,13,14,20,21,27,28,35,36:
+            if Int(cell.dayDateLabel.text!)! > 0 {
+                cell.dayDateLabel.textColor = UIColor.lightGray
+            }
+        default:
+            break
+        }
+        
+        if currentMonth == months[calendar.component(.month, from: date) - 1] && year == calendar.component(.year, from: date) && indexPath.row + 1 == day {
+            cell.backgroundColor = UIColor.red
+        }
         return cell
     }
 
 }
+
+
+
+
+
+
+
+
+
